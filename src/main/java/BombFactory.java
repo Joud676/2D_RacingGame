86962c0701e4af6e367b50706900151f597786f9
RanadWaleed <139740990+RanadWaleed@users.noreply.github.com>
2025-09-28 17:07:17 +0300

    Update BombFactory.java

--- src/main/java/BombFactory.java
+++ src/main/java/BombFactory.java
@@ -1,12 +1,17 @@
+
 public class BombFactory extends AbstractFactory {
+
+    public  Bomb getBomb( int x,int y,String difficulty){
+        if(difficulty == null){
+            return null; }
+        else if (difficulty.equalsIgnoreCase("easy")) {
+            return new Stone(x,y);
+        } else if (difficulty.equalsIgnoreCase("medium")) {
+            return new Missiles(x,y);
+        }
+        return null;
+    }
+
     public Shot getShot(String shotType){
+        return null;}}
 
-        return null;
-    }
-
-    public Bomb getBomb(){
-        return null;
-    }
-
-
-}
